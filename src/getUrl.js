26a99639f6c8a55bb6064a3ef4886726d8661fb0
wathil <yannick.garcia@icloud.com>
2022-01-20 13:51:57 +0100

    test 2

--- src/getUrl.js
+++ src/getUrl.js
@@ -2,6 +2,7 @@ const URL_START = 'https://pano225github.herokuapp.com';
 // const URL_START = 'https://warm-taiga-19057.herokuapp.com';
 // const URL_START = "http://localhost:8080";
 
+
 export const getURLStart = () => {
   return URL_START;
 }
